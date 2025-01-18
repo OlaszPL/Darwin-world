e9c418ed2a688ce7dfb270653adcd63944dacf51
Aleksander Jóźwik <aleksanderjx@gmail.com>
2025-01-18 14:10:38 +0100

    New images for animals and plants

--- src/main/java/agh/ics/darwin/model/WorldElementBox.java
+++ src/main/java/agh/ics/darwin/model/WorldElementBox.java
@@ -1,8 +1,6 @@
 package agh.ics.darwin.model;
 
-import agh.ics.darwin.model.WorldElement;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -20,8 +18,8 @@ public class WorldElementBox extends VBox {
     public WorldElementBox(WorldElement element) {
         Image image = loadImage(element.getResourceName());
         ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(40);
-        imageView.setFitWidth(40);
+        imageView.setFitHeight(35);
+        imageView.setFitWidth(35);
         imageView.setPreserveRatio(true);
         this.getChildren().add(imageView);
         this.setAlignment(Pos.CENTER);
