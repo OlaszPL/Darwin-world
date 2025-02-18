ebcb8b2fbf0c0ed2330794cc24240124aa08fe03
Aleksander Jóźwik <aleksanderjx@gmail.com>
2025-02-18 14:54:59 +0100

    Added possibility of building .jar file (multiplatform). Improved compatibility with presets.

--- src/main/java/agh/ics/darwin/presenter/PresetCreator.java
+++ src/main/java/agh/ics/darwin/presenter/PresetCreator.java
@@ -5,14 +5,22 @@ import agh.ics.darwin.model.util.PresetsWriter;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 public class PresetCreator {
+    private static final String PRESETS_DIR = System.getProperty("user.home") + "/.darwin-world/presets/";
+    private static final String DEFAULT_PRESETS_DIR = "/presets/";
 
     public static void savePreset(StartWindow startWindow, Stage stage) {
+        createPresetsDirectoryIfNeeded();
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Save Preset");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
-        fileChooser.setInitialDirectory(new File("src/main/resources/presets"));
+        fileChooser.setInitialDirectory(new File(PRESETS_DIR));
         File file = fileChooser.showSaveDialog(stage);
 
         if (file != null) {
@@ -21,10 +29,13 @@ public class PresetCreator {
     }
 
     public static void loadPreset(StartWindow startWindow, Stage stage) {
+        createPresetsDirectoryIfNeeded();
+        copyDefaultPresetsIfNeeded();
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Load Preset");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
-        fileChooser.setInitialDirectory(new File("src/main/resources/presets"));
+        fileChooser.setInitialDirectory(new File(PRESETS_DIR));
         File file = fileChooser.showOpenDialog(stage);
 
         if (file != null) {
@@ -38,4 +49,50 @@ public class PresetCreator {
             }
         }
     }
+
+    private static void createPresetsDirectoryIfNeeded() {
+        File dir = new File(PRESETS_DIR);
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+                System.out.println("Cannot initialize presets folder: " + PRESETS_DIR);
+            }
+        }
+    }
+
+    private static void copyDefaultPresetsIfNeeded() {
+        try {
+            File presetsDir = new File(PRESETS_DIR);
+            if (presetsDir.listFiles() == null || Objects.requireNonNull(presetsDir.listFiles()).length == 0) {
+                copyPresetsFromJar();
+            }
+        } catch (IOException e) {
+            System.out.println("Default presets initialization error: " + e.getMessage());
+        }
+    }
+
+    private static void copyPresetsFromJar() throws IOException {
+        try (InputStream listStream = PresetCreator.class.getResourceAsStream(DEFAULT_PRESETS_DIR + "list.txt")) {
+            if (listStream == null) {
+                System.out.println("Lack of list.txt file in resources: " + DEFAULT_PRESETS_DIR);
+                return;
+            }
+            try (BufferedReader listReader = new BufferedReader(new InputStreamReader(listStream))) {
+                String presetFileName;
+                while ((presetFileName = listReader.readLine()) != null) {
+                    presetFileName = presetFileName.trim();
+                    if (presetFileName.isEmpty()) {
+                        continue;
+                    }
+                    try (InputStream presetStream = PresetCreator.class.getResourceAsStream(DEFAULT_PRESETS_DIR + presetFileName)) {
+                        if (presetStream != null) {
+                            Files.copy(presetStream, Paths.get(PRESETS_DIR, presetFileName), StandardCopyOption.REPLACE_EXISTING);
+                        } else {
+                            System.out.println("Cannot copy preset file: " + DEFAULT_PRESETS_DIR + presetFileName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
